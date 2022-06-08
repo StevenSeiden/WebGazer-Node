4086039ef4f313fe9821ffec1d773808e3a863d0
Steven Seiden <53193445+StevenSeiden@users.noreply.github.com>
2022-06-08 16:04:46 -0500

    Removed size limitation of data being exported

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 3003
 const d = new Date();
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser())
+//app.use(bodyParser())
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
 
 
 app.get('/', function (req, res) {
